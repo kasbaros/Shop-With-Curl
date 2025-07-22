40b9971fe893ab799f183a25d93e8675ce14aaa3
bkasule <bkasule@fltug.com>
2025-07-22 12:12:15 +0300

    Include compiled assets again after resolving a serious issue

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,42 @@
-import {
-    defineConfig
-} from 'vite';
+// import {
+//     defineConfig
+// } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from "@tailwindcss/vite";
+//
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+//     server: {
+//         cors: true,
+//     },
+//     build: {
+//         outDir: 'public/build',
+//         manifest: true,
+//     },
+// });
+
+
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDirectory: 'public',
+            buildDirectory: 'build',
         }),
-        tailwindcss(),
     ],
-    server: {
-        cors: true,
-    },
     build: {
         outDir: 'public/build',
-        manifest: true,
+        manifest: 'manifest.json', // Explicitly set manifest file name
+        emptyOutDir: true, // Clear build directory before generating
     },
 });
