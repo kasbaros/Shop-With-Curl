5a7230c967fde9daae803688a9f6c7fe2d529b0c
bkasule <bkasule@fltug.com>
2025-08-26 14:01:28 +0300

    Fixing cpanel blocking of public/storage/media folder that was preventing images from showing up: 11

--- app/Traits/HasStorageImages.php
+++ app/Traits/HasStorageImages.php
@@ -20,6 +20,56 @@
         }
 
         /**
+         * Override Spatie's getFirstMediaUrl to use our storage system
+         */
+        public function getFirstMediaUrl(string $collectionName = 'default', string $conversionName = ''): string
+        {
+            return $this->getMediaStorageUrl($collectionName, $conversionName);
+        }
+
+        /**
+         * Get Media URL for Spatie Media Library (if used)
+         */
+        public function getMediaStorageUrl(string $collection = 'images', string $conversion = '', int $index = 0): string
+        {
+            if (!method_exists($this, 'getMedia')) {
+                return $this->getPlaceholderImage();
+            }
+
+            $media = $this->getMedia($collection)->get($index);
+
+            if (!$media) {
+                return $this->getPlaceholderImage();
+            }
+
+            // For Spatie media, we'll store in media subfolder
+            $path = 'media/' . $media->id . '/' . $media->file_name;
+
+            if ($conversion === 'thumb') {
+                return ImageStorageHelper::url($path);
+            } elseif ($conversion === 'large') {
+                return ImageStorageHelper::url($path);
+            }
+
+            return ImageStorageHelper::url($path);
+        }
+
+        /**
+         * Get all media URLs from a collection
+         */
+        public function getAllMediaStorageUrls(string $collection = 'images', ?string $conversion = null): array
+        {
+            if (!method_exists($this, 'getMedia')) {
+                return [];
+            }
+
+            return $this->getMedia($collection)->map(function ($media) use ($conversion) {
+                $path = 'media/' . $media->id . '/' . $media->file_name;
+                return ImageStorageHelper::url($path);
+            })->toArray();
+        }
+
+        /**
          * Store an image for this model
          */
         public function storeImage(UploadedFile $file, string $attribute = 'image'): string
@@ -67,7 +117,11 @@
                 'product' => 'images/placeholder-product.jpg',
                 'category' => 'images/placeholder-category.jpg',
                 'gallery' => 'images/placeholder-gallery.jpg',
+                'galleryitem' => 'images/placeholder-gallery.jpg',
                 'user' => 'images/placeholder-user.jpg',
+                'banner' => 'images/placeholder-banner.jpg',
+                'lookbook' => 'images/placeholder-lookbook.jpg',
+                'promobanner' => 'images/placeholder-banner.jpg',
                 'default' => 'images/placeholder.jpg',
             ];
 
