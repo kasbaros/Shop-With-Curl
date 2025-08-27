139c62233475a80357dd44fbfe461348f6112f14
bkasule <bkasule@fltug.com>
2025-08-27 16:22:49 +0300

    Fixing cpanel blocking of public/storage/media folder that was preventing images from showing up: 52

--- resources/views/livewire/guest/shop/shop-grid.blade.php
+++ resources/views/livewire/guest/shop/shop-grid.blade.php
@@ -1,109 +1,3 @@
-{{--<div class="space-y-6">--}}
-{{--    <div class="tf-page-title">--}}
-{{--        <div class="container-full">--}}
-{{--            <div class="heading text-center">Check out our shop catalog</div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <section class="flat-spacing-1">--}}
-{{--        <div class="container">--}}
-{{--            <div class="tf-grid-layout xl-col-3 tf-col-2">--}}
-{{--                <div class="collection-item hover-img">--}}
-{{--                    <div class="collection-inner">--}}
-{{--                        <a href="shop-default.html" class="collection-image img-style">--}}
-{{--                            <img class=" ls-is-cached lazyloaded" data-src="images/collections/full-shot-black-woman-training-outdoors.jpg"--}}
-{{--                                 src="images/collections/full-shot-black-woman-training-outdoors.jpg" alt="collection-img">--}}
-{{--                        </a>--}}
-{{--                        <div class="collection-content">--}}
-{{--                            <a href="shop-default.html" class="tf-btn collection-title hover-icon"><span>Gym/Shape Wear</span><i--}}
-{{--                                    class="icon icon-arrow1-top-left"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="collection-item hover-img">--}}
-{{--                    <div class="collection-inner">--}}
-{{--                        <a href="shop-default.html" class="collection-image img-style">--}}
-{{--                            <img class=" ls-is-cached lazyloaded" data-src="images/collections/collection-9.jpg"--}}
-{{--                                 src="images/collections/collection-9.jpg" alt="collection-img">--}}
-{{--                        </a>--}}
-{{--                        <div class="collection-content">--}}
-{{--                            <a href="shop-default.html" class="tf-btn collection-title hover-icon"><span>Jackets</span><i--}}
-{{--                                    class="icon icon-arrow1-top-left"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="collection-item hover-img">--}}
-{{--                    <div class="collection-inner">--}}
-{{--                        <a href="shop-default.html" class="collection-image img-style">--}}
-{{--                            <img class=" ls-is-cached lazyloaded" data-src="images/collections/collection-10.jpg"--}}
-{{--                                 src="images/collections/collection-10.jpg" alt="collection-img">--}}
-{{--                        </a>--}}
-{{--                        <div class="collection-content">--}}
-{{--                            <a href="shop-default.html" class="tf-btn collection-title hover-icon"><span>Bras</span><i--}}
-{{--                                    class="icon icon-arrow1-top-left"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="collection-item hover-img">--}}
-{{--                    <div class="collection-inner">--}}
-{{--                        <a href="shop-default.html" class="collection-image img-style">--}}
-{{--                            <img class=" ls-is-cached lazyloaded" data-src="images/collections/collection-11.jpg"--}}
-{{--                                 src="images/collections/collection-11.jpg" alt="collection-img">--}}
-{{--                        </a>--}}
-{{--                        <div class="collection-content">--}}
-{{--                            <a href="shop-default.html" class="tf-btn collection-title hover-icon"><span>Leggings</span><i--}}
-{{--                                    class="icon icon-arrow1-top-left"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="collection-item hover-img">--}}
-{{--                    <div class="collection-inner">--}}
-{{--                        <a href="shop-default.html" class="collection-image img-style">--}}
-{{--                            <img class=" ls-is-cached lazyloaded" data-src="images/collections/collection-12.jpg"--}}
-{{--                                 src="images/collections/collection-12.jpg" alt="collection-img">--}}
-{{--                        </a>--}}
-{{--                        <div class="collection-content">--}}
-{{--                            <a href="shop-default.html" class="tf-btn collection-title hover-icon"><span>Jump Suits</span><i--}}
-{{--                                    class="icon icon-arrow1-top-left"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="collection-item hover-img">--}}
-{{--                    <div class="collection-inner">--}}
-{{--                        <a href="shop-default.html" class="collection-image img-style">--}}
-{{--                            <img class=" ls-is-cached lazyloaded" data-src="images/collections/collection-13.jpg"--}}
-{{--                                 src="images/collections/collection-13.jpg" alt="collection-img">--}}
-{{--                        </a>--}}
-{{--                        <div class="collection-content">--}}
-{{--                            <a href="shop-default.html" class="tf-btn collection-title hover-icon"><span>Bags</span><i--}}
-{{--                                    class="icon icon-arrow1-top-left"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <!-- pagination -->--}}
-{{--            <ul class="tf-pagination-wrap tf-pagination-list">--}}
-{{--                <li class="active">--}}
-{{--                    <a href="#" class="pagination-link">1</a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="#" class="pagination-link animate-hover-btn">2</a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="#" class="pagination-link animate-hover-btn">3</a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="#" class="pagination-link animate-hover-btn">4</a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="#" class="pagination-link animate-hover-btn">--}}
-{{--                        <span class="icon icon-arrow-right"></span>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--            </ul>--}}
-{{--        </div>--}}
-{{--    </section>--}}
-{{--</div>--}}
-
 <div class="space-y-6">
     <!-- Breadcrumbs -->
     <livewire:components.breadcrumbs />
@@ -183,14 +77,25 @@
 
                     <div class="row row-cols-2 row-cols-md-3 g-4">
                         @forelse($products as $product)
-                            @php
-                                $media = $product->media->first();
-                                $img = $media && method_exists($media, 'getUrl') ? $media->getUrl() : ($media->url ?? null);
-                                $imageUrl = $img ?: $productFallback;
+{{--                            @php--}}
+{{--                                $media = $product->media->first();--}}
+{{--                                $img = $media && method_exists($media, 'getUrl') ? $media->getUrl() : ($media->url ?? null);--}}
+{{--                                $imageUrl = $img ?: $productFallback;--}}
 
-                                $hasSale = !is_null($product->sale_price) && $product->sale_price < $product->price;
-                                $displayPrice = $hasSale ? $product->sale_price : $product->price;
-                                $format = fn($p) => (is_int($p) || ctype_digit((string)$p)) ? number_format($p / 100, 2) : number_format((float)$p, 2);
+{{--                                $hasSale = !is_null($product->sale_price) && $product->sale_price < $product->price;--}}
+{{--                                $displayPrice = $hasSale ? $product->sale_price : $product->price;--}}
+{{--                                $format = fn($p) => (is_int($p) || ctype_digit((string)$p)) ? number_format($p / 100, 2) : number_format((float)$p, 2);--}}
+{{--                            @endphp--}}
+
+                            @php
+                                // Use the same robust image logic as the home page for consistency.
+                                $galleryPaths = is_array($product->gallery) ? $product->gallery : [];
+                                $primaryPath = $galleryPaths[0] ?? $product->featured_image;
+                                $img = $product->getStorageImageUrl($primaryPath);
+
+                                // The hover image is the featured_image, falling back to the primary image if it doesn't exist.
+                                $hoverPath = $product->featured_image ?? $primaryPath;
+                                $hover = $product->getStorageImageUrl($hoverPath);
                             @endphp
 
                             <div class="col">
